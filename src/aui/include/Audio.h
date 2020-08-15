49a9e7890d741f1b2199c5625ce8053a5accd3cf
striderssoftware <strider@shadowsoftware.biz>
2020-08-15 12:40:57 -0700

    + New aui Audio using SDL2. + Audio.h has been re-written to use the Synth class. + Audio.h does NOT suport audio input functionality. Old code has been removed, new code comming. + A Synth and supporting classes now implement the "playing" of sounds
    Note:

--- src/aui/include/Audio.h
+++ src/aui/include/Audio.h
@@ -19,22 +19,15 @@
 #define AUDIO_H
 
 
-//#define DOAUDIO;
-//#define DOAUDIOINPUT;
-//#define DOAUDIOLOGGING;
-//#define AUDIONOTYET;
+#define DOAUDIO
+//#define DOAUDIOINPUT
+#define DOAUDIOLOGGING
+//#define AUDIONOTYET
 
 #include <iostream>
-
-//added for pipes
-#include <iostream>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <string.h>
 
 #include "AuiConstants.h"
+#include "Synth.h"
 
 using namespace std;
 
@@ -42,7 +35,6 @@ using namespace std;
 
 //TODO VDT - get rid of releative paths?
 #include "../../core/include/Logger.h"
-
 
 namespace MFM
 {
@@ -53,9 +45,7 @@ namespace MFM
 
   public:
 
-    Audio() :
-      m_fd(-1),
-      m_micfp(NULL)
+    Audio()
     {
 #ifdef DOAUDIOINPUT
       if ( initialize )
@@ -63,8 +53,6 @@ namespace MFM
 #ifdef DOAUDIOLOGGING
       LOG.Message("Audio: constructor was called");
 #endif
-	  CreatePipe();
-	  StartMicMonitor();
 	  initialize = false;
 	}
 #endif
@@ -72,8 +60,6 @@ namespace MFM
 
     virtual ~Audio()
     {
-      close(m_fd);
-      pclose(m_micfp);
     }
 
     /**
@@ -88,42 +74,7 @@ namespace MFM
 #endif
       return true;
     }
-
     
-    /**
-     * This will start a new process to listen to the MIC
-     * 
-     */
-    bool StartMicMonitor()
-    {
-      LOG.Message("Starting the Mic Monitor EXE");
-
-      m_micfp = popen(MIC_MONITOR_EXE_PATH, "r");
-
-      if ( m_micfp == NULL )
-	{
-#ifdef DOAUDIOLOGGING
-	  LOG.Message("The Monitor exe failed to start");
-#endif
-	  return false;
-	}
-      
-      return true;
-    }
-    
-    /**
-     * This will create a named pipe.
-     */
-    bool CreatePipe()
-    {
-      /* Create the FIFO if it does not exist */
-      //umask(0);
-      mknod(FIFO_FILE, S_IFIFO|0777, 0);
-      
-      return true;
-    }
-    
-
     /**
      * This takes a u32 sound attribute maps that to wav files and plays them.
      * Currentlly used to play an Elements audio attribute.
@@ -135,7 +86,7 @@ namespace MFM
       //LOG.Message("uSound was  %x", static_cast<int>(uSound));
 #endif
       
-      return true; //PlaySound(uSound);
+      return true; // TODO SDL2PORT - uSound to double - PlaySound(uSound);
     }
 
     
@@ -151,11 +102,11 @@ namespace MFM
 #ifdef DOAUDIOLOGGING
       LOG.Message("Audio:GetAudio - was called");
 #endif
-      //system("arecord -D hw:0,0 -c1 -d 0 -r 22050 -f S16_LE");  //TODO VDT get output into the stream m_pMicInput
-      system("arecord -D hw:0,0 -c1 -d 1 -r 22050 -f S16_LE test.wav");  //TODO VDT get output into the stream m_pMicInput
+
 #endif
 #endif
-      return m_pMicInput;
+      istream* pMicInput;
+      return pMicInput;
     }
 
     
@@ -165,53 +116,8 @@ namespace MFM
      */
     bool CheckForAudioEvent()
     {
-#ifdef DOAUDIOINPUT
-      int fd;
-      char readbuf[11]; //TODO the messages (dosomething) are "pings". They should be of a fixed size.
-      int read_bytes;
-
-      //LOG.Message("Audio:CheckForAudioEvent - was called");
-
-      if ( m_fd == -1 )
-	{
-	  m_fd = open(FIFO_FILE, O_RDONLY);
-	  fcntl(m_fd, F_SETFL, O_NONBLOCK);
-	}
-      if ( m_fd < 0 )
-	{
-	  // TODO VDT log error
-	  LOG.Message("Audio::CheckForAudioEvent  - open failed errno was: %i", errno);
-	  return false;
-	}
-      
-      read_bytes = read(m_fd, readbuf, sizeof(readbuf));
-      if ( read_bytes < 0 )
-	{
-	  if ( errno != EAGAIN )
-	    {
-	      // TODO ERROR CASE, stop this, stop monitor??
-	      LOG.Message("Audio::CheckForAudioEvent -  error");
-	    }
-	  
-	  return false;
-	}
-      else if ( read_bytes == 0 )
-	{
-	   LOG.Message("Audio::CheckForAudioEvent - no bytes to read");
-	  return false;
-	}
-
-      readbuf[read_bytes] = '\0';
-      LOG.Message("Received a message in namedPipe");
-
-      
-      //TODO the messages are "pings", no need to check message contents.
-      return true;  // There WAS an audioEvent
-
-#else
-
       return false;
-#endif
+
     }
     
   private:
@@ -223,49 +129,17 @@ namespace MFM
     bool PlaySound(u32 uSound)
     {
 #ifdef DOAUDIO
-      // TODO VDT - development code it will go away!                                                                                          
-      // TODO VDT - sorry about the paths, fix them to point into your distro.        
-
-      if ( uSound == 0u )
-        {
-          return true;
-        }
-      else if ( uSound == (u32)0xff000011 )
-        {
-          system("aplay -d 1 ~/code/T2ProjectCodeRepo/MFM/res/audio/aMinorScale/A.wav");
-          return true;
-        }
-      else if ( uSound == (u32)0xff000033 )
-        {
-          system("aplay -d 1 ~/code/T2ProjectCodeRepo/MFM/res/audio/aMinorScale/C.wav");
-          return true;
-        }
-      else if ( uSound == (u32)0xff000055 )
-        {
-          system("aplay -d 1 ~/code/T2ProjectCodeRepo/MFM/res/audio/aMinorScale/E.wav");
-          return true;
-        }
-      else if ( uSound == (u32)0xff000077 )
-        {
-          system("aplay -d 1 ~/code/T2ProjectCodeRepo/MFM/res/audio/aMinorScale/G#.wav");
-          return true;
-        }
-      else
-        {
           LOG.Message("uSound was  %x", static_cast<int>(uSound));
           // TODO VDT play a sound  i.e. -  aplay wavFiles/error.wav
-	}
-#endif      
-                return true;
+
+	  m_synth.AddSound(uSound);
+	  m_synth.BeginPlaying();
+#endif      	  
+	  return true;
       
     }//END play sound    
 
-   
-    
-    int m_fd;
-    FILE* m_micfp;
-    istream* m_pMicInput;
-    
+    Synth m_synth;
   };
 
 } /* namespace MFM */
