9c4452e7d870eae3fd316d49502396c7f42afdaf
striderssoftware <strider@shadowsoftware.biz>
2020-08-19 12:29:47 -0700

    + Refactor the Audio files to have implementation in the cpp file.
    Note:

--- src/aui/include/Audio.h
+++ src/aui/include/Audio.h
@@ -1,7 +1,3 @@
-/*                                              -*- mode:C++ -*-
-  Audio.h 
-*/
-
 /**
   \file Audio.h 
   \ ADDED VTD
@@ -14,126 +10,62 @@
   \ more of using a synthizer to create these sounds and so the synthesizer would be to sound 
   \ what the screen is to visual aspects.  
  */
-
 #ifndef AUDIO_H
 #define AUDIO_H
 
-
-#define DOAUDIO
-//#define DOAUDIOINPUT
-#define DOAUDIOLOGGING
-//#define AUDIONOTYET
-
 #include <iostream>
-
 #include "AuiConstants.h"
 #include "Synth.h"
-
-using namespace std;
-
 //#include "FileByteSink.h" // For STDERR
-
 //TODO VDT - get rid of releative paths?
 #include "../../core/include/Logger.h"
 
+using namespace std;
+
 namespace MFM
 {
-
+  
   class Audio 
   {
-
+    
   public:
-
-    Audio()
-    {
-#ifdef DOAUDIOLOGGING
-      LOG.Message("Audio: constructor was called");
-#endif
-      m_synth.Init();
-      m_synth.BeginRecording();
-    }
-
-    virtual ~Audio()
-    {
-    }
-
+    
+    Audio();
+    
+    virtual ~Audio();
+    
     /**
      * A test function
      *
      */
-    bool AuiTestFunction(u32 uSound)
-    {
-#ifdef DOAUDIOLOGGING
-      LOG.Message("Audio:  AuiTestFunction - was called");
-      LOG.Message("uSound was  %x", static_cast<int>(uSound));
-#endif
-      return true;
-    }
-    
+    bool AuiTestFunction(u32 uSound);
+        
     /**
      * This takes a u32 sound attribute maps that to wav files and plays them.
      * Currentlly used to play an Elements audio attribute.
      */
-    bool ProcessAudio(u32 uSound)
-    {
-#ifdef DOAUDIOLOGGING
-      LOG.Message("Audio:  ProcessAudio - was called");
-      LOG.Message("uSound was  %x", static_cast<int>(uSound));
-#endif
-      
-      return PlaySound(uSound); // TODO SDL2PORT - uSound to double - PlaySound(uSound);
-    }
-
-    
+    bool ProcessAudio(u32 uSound);
+        
     /**
      * This WILL return an input stream of MIC data.
      *
      */
-    istream*  GetAudio()
-    {
-#ifdef AUDIONOTYET
-#ifdef DOAUDIOINPUT
-      
-#ifdef DOAUDIOLOGGING
-      LOG.Message("Audio:GetAudio - was called");
-#endif
-
-#endif
-#endif
-      istream* pMicInput;
-      return pMicInput;
-    }
-
+    istream*  GetAudio();
     
     /**
      * This returns if there has been an Audio Event,
      * Currentlly an Audio Event is defined as MIC input above a volume threshold
      */
-    bool CheckForAudioEvent()
-    {
-      return m_synth.CheckForAudioEvent();
-    }
-
-    
+    bool CheckForAudioEvent();
+        
   private:
 
     /**
      * This makes a system call to play a wav file.
      * currently this maps u32 values to wav files that are musical notes in the Western Tonal system. 
      */
-    bool PlaySound(u32 uSound)
-    {
-#ifdef DOAUDIO
-          LOG.Message("Audio::PlaySound uSound was  %x", static_cast<int>(uSound));
-          // TODO VDT play a sound  i.e. -  aplay wavFiles/error.wav
-
-	  m_synth.AddSound(uSound);
-	  m_synth.BeginPlaying();
-#endif      	  
-	  return true;
-      
-    }//END play sound    
-
+    bool PlaySound(u32 uSound);
+    
     Synth m_synth;
   };
 
--- src/aui/src/Audio.cpp
+++ src/aui/src/Audio.cpp
@@ -1,2 +1,105 @@
 #include "Audio.h"
 
+#include <iostream>
+#include "AuiConstants.h"
+#include "Synth.h"
+//#include "FileByteSink.h" // For STDERR
+//TODO VDT - get rid of releative paths?
+#include "../../core/include/Logger.h"
+
+#define DOAUDIO
+//#define DOAUDIOINPUT
+#define DOAUDIOLOGGING
+//#define AUDIONOTYET
+
+using namespace std;
+
+MFM::Audio::Audio()
+{
+#ifdef DOAUDIOLOGGING
+  LOG.Message("Audio: constructor was called");
+#endif
+  m_synth.Init();
+  m_synth.BeginRecording();
+}
+
+MFM::Audio::~Audio()
+{
+}
+
+/**
+ * A test function
+ *
+ */
+bool MFM::Audio::AuiTestFunction(u32 uSound)
+{
+#ifdef DOAUDIOLOGGING
+  LOG.Message("Audio:  AuiTestFunction - was called");
+  LOG.Message("uSound was  %x", static_cast<int>(uSound));
+#endif
+  return true;
+}
+
+/**
+ * This takes a u32 sound attribute maps that to wav files and plays them.
+ * Currentlly used to play an Elements audio attribute.
+ */
+bool MFM::Audio:: ProcessAudio(u32 uSound)
+{
+#ifdef DOAUDIOLOGGING
+  LOG.Message("Audio:  ProcessAudio - was called");
+  LOG.Message("uSound was  %x", static_cast<int>(uSound));
+#endif
+  
+  return PlaySound(uSound); // TODO SDL2PORT - uSound to double - PlaySound(uSound);
+}
+
+
+/**
+ * This WILL return an input stream of MIC data.
+ *
+ */
+istream*  MFM::Audio::GetAudio()
+{
+#ifdef AUDIONOTYET
+#ifdef DOAUDIOINPUT
+  
+#ifdef DOAUDIOLOGGING
+  LOG.Message("Audio:GetAudio - was called");
+#endif
+  
+#endif
+#endif
+  istream* pMicInput;
+  return pMicInput;
+}
+
+
+/**
+ * This returns if there has been an Audio Event,
+ * Currentlly an Audio Event is defined as MIC input above a volume threshold
+ */
+bool MFM::Audio::CheckForAudioEvent()
+{
+  return m_synth.CheckForAudioEvent();
+}
+
+
+
+/**
+ * This makes a system call to play a wav file.
+ * currently this maps u32 values to wav files that are musical notes in the Western Tonal system. 
+ */
+bool MFM::Audio::PlaySound(u32 uSound)
+{
+#ifdef DOAUDIO
+  LOG.Message("Audio::PlaySound uSound was  %x", static_cast<int>(uSound));
+  // TODO VDT play a sound  i.e. -  aplay wavFiles/error.wav
+  
+  m_synth.AddSound(uSound);
+  m_synth.BeginPlaying();
+#endif      	  
+  return true;
+  
+}//END play sound    
+
