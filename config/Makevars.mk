53212cc889d80fcf23d356e1bd0eb28de81c64ee
striderssoftware <strider@shadowsoftware.biz>
2021-04-05 19:00:01 -0600

    + Ifdef on platform the use of c++11
    Note:

--- config/Makevars.mk
+++ config/Makevars.mk
@@ -68,8 +68,10 @@ COMMON_LDFLAGS+=-Wl,--fatal-warnings -pthread
 # Urgh gcc 8.3 warns about parens I want to keep
 COMMON_CPPFLAGS+=-Wno-parentheses
 
-# HEAD repo does NOT have this c++11 cpp flag.
+# Needed for AUI lib - goes with the HAVE_AUDIO Define
+ifneq ($(PLATFORM),tile)
 COMMON_CPPFLAGS+= -std=c++11
+endif
 
 
 # Ubuntu 12.04 needs this for clock_gettime
